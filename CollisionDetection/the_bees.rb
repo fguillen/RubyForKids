05fcf029a48d5fb9fd95c85cbe4d0dbaf3634d1c
Fernando Guillen <fguillen.mail@gmail.com>
2021-06-25 19:41:14 +0200

    More comments in bee project

--- CollisionDetection/the_bees.rb
+++ CollisionDetection/the_bees.rb
@@ -4,7 +4,7 @@ require "gosu"
 # The Bee class
 class Bee
   def initialize
-    @image = Gosu::Image.new("images/bee.png");
+    @image = Gosu::Image.new("#{__dir__}/images/bee.png");
     @x = 600
     @y = 50
     @width = 200
@@ -17,15 +17,16 @@ class Bee
   end
 
   def draw
-    @image.draw(@x, @y, 2)
+    @image.draw(@x, @y, 20)
+
+    # You can uncomment this line to see the Bee square
     # debug
   end
 
   # The debug method to see the box
   def debug
-    Gosu.draw_rect(@x, @y, @width, @height, Gosu::Color::YELLOW, 0)
+    Gosu.draw_rect(@x, @y, @width, @height, Gosu::Color::YELLOW, 10)
   end
-
 
   # These methods are to make instance variables accessible
   # for another instances like the Person instance
@@ -44,7 +45,6 @@ class Bee
   def height
     return @height
   end
-
 
   # This is the same logic we have implemented in other
   # games, nothing new here
@@ -74,8 +74,8 @@ end
 
 class Person
   def initialize(bee)
-    @image_happy = Gosu::Image.new("images/person_happy.png");
-    @image_unhappy = Gosu::Image.new("images/person_unhappy.png");
+    @image_happy = Gosu::Image.new("#{__dir__}/images/person_happy.png");
+    @image_unhappy = Gosu::Image.new("#{__dir__}/images/person_unhappy.png");
     @x = 150
     @y = 200
     @width = 228
@@ -85,7 +85,7 @@ class Person
   end
 
   def update
-    if(collision_with_bee)
+    if collision_with_bee
       @status = "unhappy"
     else
       @status = "happy"
@@ -93,31 +93,32 @@ class Person
   end
 
   def draw
-    if(@status == "happy")
-      @image_happy.draw(@x, @y, 1)
+    if @status == "happy"
+      @image_happy.draw(@x, @y, 15)
     else
-      @image_unhappy.draw(@x, @y, 1)
+      @image_unhappy.draw(@x, @y, 15)
     end
 
+    # You can uncomment this line to see the Person square
     # debug
   end
 
   # The debug method to see the box
   def debug
-    Gosu.draw_rect(@x, @y, @width, @height, Gosu::Color::YELLOW, 0)
+    Gosu.draw_rect(@x, @y, @width, @height, Gosu::Color::YELLOW, 10)
   end
 
   # This is the core of the algorithm
   # I took this implementation from here:
   # https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
   def collision_with_bee
-    if(
+    if (
       @bee.x < (@x + @width) &&
       (@bee.x + @bee.width) > @x &&
       @bee.y < (@y + @height) &&
       @bee.y + @bee.height > @y
     )
-       return true
+      return true
     else
       return false
     end
@@ -126,11 +127,15 @@ end
 
 # This is the main class the Game class
 class Game < Gosu::Window
+  WIDTH = 1000
+  HEIGHT = 800
+
   def initialize
-    super 1000, 800 # the size of the Window
+    super WIDTH, HEIGHT # the size of the Window
 
     @bee = Bee.new # our "bee" instance
     @person = Person.new(@bee) # notice that I pass the "bee" instance to the "person" instance in the constructor
+    @font = Gosu::Font.new(20, name: Gosu.default_font_name)
   end
 
   def update
@@ -139,9 +144,24 @@ class Game < Gosu::Window
   end
 
   def draw
+    draw_background
     @bee.draw
     @person.draw
+    draw_instructions
+  end
+
+  def draw_background
+    # Drawing a background color
+    # See the doc of Gosu::Color constructor: https://www.rubydoc.info/github/gosu/gosu/master/Gosu%2FColor:initialize
+    Gosu.draw_rect(0, 0, WIDTH, HEIGHT, Gosu::Color.new(255, 254, 197, 187), 0)
+  end
+
+  def draw_instructions
+    # See the documentation for Gosu::Font.draw_text:
+    # https://www.rubydoc.info/github/gosu/gosu/master/Gosu%2FFont:draw_text
+    @font.draw_text("Use the cursors to move the bee and scare the person", 400, 750, 0, 1, 1, Gosu::Color::BLACK)
   end
 end
 
+# Start the game
 Game.new.show
